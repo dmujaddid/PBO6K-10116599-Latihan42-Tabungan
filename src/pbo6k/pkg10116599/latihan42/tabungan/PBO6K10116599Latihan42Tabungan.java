ce5f426132777d8be550bc51848fea6fddd93c91
Roby <robyeka@email.unikom.ac.id>
2018-10-24 04:44:11 +0700

    ngoding

--- src/pbo6k/pkg10116599/latihan42/tabungan/PBO6K10116599Latihan42Tabungan.java
+++ src/pbo6k/pkg10116599/latihan42/tabungan/PBO6K10116599Latihan42Tabungan.java
@@ -5,9 +5,16 @@
  */
 package pbo6k.pkg10116599.latihan42.tabungan;
 
+import java.util.Scanner;
+
 /**
  *
- * @author eka
+ * @author 
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program : 
+ * 
  */
 public class PBO6K10116599Latihan42Tabungan {
 
@@ -16,6 +23,23 @@ public class PBO6K10116599Latihan42Tabungan {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+            
+        Scanner scan1 = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+        Tabungan tabungan = new Tabungan();
+
+        System.out.println("==== Program Penarikan Uang ====");
+        System.out.print("Masukkan Saldo Awal\t: " );
+        
+        int saldo = scan1.nextInt();
+        tabungan.tabungan(saldo);
+       
+        
+        System.out.print("Jumlah uang yang diambil: ");
+        int jumlah = scan2.nextInt();
+        saldo = tabungan.tabungan(saldo) - tabungan.ambilUang(jumlah);        
+        System.out.println("Saldo Anda sekarang\t: " +saldo);
+        
     }
     
 }
---
+++ src/pbo6k/pkg10116599/latihan42/tabungan/Tabungan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan42.tabungan;
+
+/**
+ *
+ * @author eka
+ */
+public class Tabungan extends PBO6K10116599Latihan42Tabungan {
+    
+    private int saldo;
+    
+    
+    public int tabungan(int saldo) {
+         return saldo; 
+    }
+
+    public int ambilUang(int jumlah) {
+        return jumlah;
+    }
+    
+}
